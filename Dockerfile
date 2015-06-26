f22f612f8e53aed39d0ea1e2c898531701608872
BJTerry <bjterry@Mac-Pro.local>
2015-06-25 22:30:40 -0700

    Added requirements file, sloccount for docker.

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,12 @@ RUN apt-get update && apt-get clean
 RUN apt-get install -q -y openjdk-7-jre-headless && apt-get clean
 ADD http://mirrors.jenkins-ci.org/war/1.617/jenkins.war /opt/jenkins.war
 RUN ln -sf /jenkins /root/.jenkins
+
+# GIT
+RUN apt-get install -q -y git
+
+# SLOCCount
+RUN apt-get install -q -y sloccount
   
 ENTRYPOINT ["java", "-jar", "/opt/jenkins.war"]
 EXPOSE 8080
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_sloccount'
+)
